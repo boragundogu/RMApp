70c44b70bd9e9a7039c1b28af535e1badc6e84d1
Bora <gundogubora24@gmail.com>
2024-03-23 12:01:25 +0300

    Initial Commit

---
+++ RMApp/RMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMAppApp.swift
+//  RMApp
+//
+//  Created by Bora Gündoğu on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
